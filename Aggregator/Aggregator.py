187520da7ca07d6d31e4dab555e9e87516803646
iamhyc <sudofree@163.com>
2017-11-08 11:30:10 +0800

    add config file

--- Aggregator/Aggregator.py
+++ Aggregator/Aggregator.py
@@ -1,7 +1,25 @@
+#! /usr/bin/python
+'''
+Aggregator: for data flow manipulation
+@author: Mark Hong
+'''
+import json
+import thread
+import socket
+import binascii, struct
 
+global config
 
 def main():
-	pass
+	try:
+		pass
+	except Exception as e:
+		raise e
+	finally:
+		pass
 
 if __name__ == '__main__':
+	with open('../config.json') as cf:
+		config = json.load(cf)
+		pass
 	main()
