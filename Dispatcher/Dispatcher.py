49c40ba8246c3a85df916ab0737a577f6b1d3cd7
iamhyc <sudofree@163.com>
2017-12-08 12:48:38 +0800

    temp work * need dual TCP uni-link * need refactor Aggregator

--- Dispatcher/Dispatcher.py
+++ Dispatcher/Dispatcher.py
@@ -6,13 +6,13 @@ Dispatcher: for command manipulation
 import json
 from time import sleep, ctime
 from multiprocessing import Process, Queue
+from threading import Thread
 import socket, string, binascii
 
 from Distributor import Distributor
 from Algorithm import Algorithm
 
 global config
-global skt_res, skt_req
 global proc_map, proc_remap
 global ClientCount
 global alg_node, fb_q, c2p_q
@@ -29,7 +29,7 @@ def cmd_parse(str):
 	return op, cmd
 	pass
 
-def response(status, addr, optional=''):
+def response(status, sock, optional=''):
 	if status:
 		frame = '+'
 	else:
@@ -38,15 +38,15 @@ def response(status, addr, optional=''):
 	if optional != '':
 		frame += '\n' + optional
 	
-	skt_res.sendto(frame, (addr, config['converg_term_port']))
+	skt_res.send(frame)
 	pass
 
-def process_print(cmd, addr):
+def process_print(cmd, sock, addr):
 	proc_list = ''.join( ('%s %s\n')%(k, v['char']) for (k,v) in proc_map.items())
 	response(True, addr, proc_list)
 	pass
 
-def register_client(cmd, addr):
+def register_client(cmd, sock, addr):
 	global ClientCount
 
 	wifi_ip, vlc_ip = cmd
@@ -58,6 +58,7 @@ def register_client(cmd, addr):
 	proc_remap[wifi_ip] = task_id #revese map over Wi-Fi link
 	proc_map[task_id] = {}
 	proc_map[task_id]['char'] = (wifi_ip, vlc_ip, port)
+	proc_map[task_id]['sock'] = sock
 	proc_map[task_id]['queue'] = (p2c_q, fb_q)
 	proc_map[task_id]['_thread'] = Distributor(
 									task_id,
@@ -75,7 +76,7 @@ def register_client(cmd, addr):
 	ClientCount += 1
 	pass
 
-def remove_client(cmd, addr):
+def remove_client(cmd, sock, addr):
 	task_id = cmd[0]
 	if not proc_map.has_key(task_id):
 		raise Exception
@@ -84,22 +85,23 @@ def remove_client(cmd, addr):
 	proc_map[task_id]['_thread'].terminate() #forcely exit the server
 	del proc_map[task_id] # delete the item
 
-	response(True, addr)
+	response(True, sock)
 	pass
 
-def set_source(cmd, addr):
+def set_source(cmd, sock, addr):
 	task_id = proc_remap[addr] if cmd[0] <= -1 else cmd[0] #-1 for no id
 	if proc_map.has_key(task_id):
 		p2c_cmd = ''.join(['src'] + cmd[1:])
 		proc_map[task_id]['queue'][0].put_nowait(p2c_cmd)
-		response(True, addr)
+		response(True, sock) # to Controller Side
+		#response(True, proc_map[task_id]['char'][0]) # To Wi-Fi Side
 		pass
 	else:
 		response(False, addr)
 		pass
 	pass
 
-def start_source(cmd, addr):
+def start_source(cmd, sock, addr):
 	task_id = proc_remap[addr] if cmd[0] <= -1 else cmd[0] #-1 for no id
 	if proc_map.has_key(task_id):
 		p2c_cmd = 'src-now'
@@ -111,20 +113,30 @@ def start_source(cmd, addr):
 		pass
 	pass
 
-def idle_work(cmd, addr):
+def idle_work(cmd, sock, addr):
 	response(True, addr)
 	pass
 
 def disp_init():
-	global skt_req, skt_res, fb_q, c2p_q, alg_node, ClientCount, proc_map, proc_remap
+	global skt, fb_q, c2p_q, alg_node, ClientCount, proc_map, proc_remap, ops_map
 
 	# Map Init
 	proc_map = {}
 	proc_remap = {}
+	ops_map = {
+		# General Operation
+		"ls":process_print,
+		"add":register_client,
+		# Specific Operation
+		"rm":remove_client,
+		"src":set_source,
+		"src-now":start_source
+		"idle":idle_work
+	}
 	# converg Socket Init
-	skt_res = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	skt_req = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	skt_req.bind(('', config['converg_disp_port']))
+	skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	skt.bind(('', config['converg_disp_port']))
+	skt.listen(10)
 	# plugin Alg. Node Init 
 	ClientCount = 0
 	fb_q = Queue()
@@ -139,31 +151,30 @@ def disp_exit():
 	exit()
 	pass
 
-def main():
-	disp_init()
-	# Converg Layer Dispatcher
-	ops_map = {
-		# General Operation
-		"ls":process_print,
-		"add":register_client,
-		# Specific Operation
-		"rm":remove_client,
-		"src":set_source,
-		"src-now":start_source
-		"idle":idle_work
-	}
-
+def tcplink(sock, addr):
 	while True:
-		#skt_req.settimeout(15) #for Windows debug
-		data, addr = skt_req.recvfrom(1024)
+		data = sock.recv(1024)
 		op, cmd = cmd_parse(data)
 		try:
-			ops_map[op](cmd, addr[0])
+			ops_map[op](cmd, sock, addr)
 		except Exception as e:
 			print('\nErrorCode: %s'%(e))
 			print('\"%s\" from %s'%(data, addr))
-			skt_res.sendto(op+' Failed', (addr, config['converg_term_port']))
+			sock.send(op+' Failed')
+			sock.close()
 			pass
+		pass
+		pass
+
+def main():
+	disp_init()
+	# Converg Layer Dispatcher
+
+	while True:
+		#skt_req.settimeout(15) #for Windows debug
+		sock, addr = skt.accept()
+		t = Thread(target=tcplink, args=(sock, addr))
+    	t.start()
 		pass
 	pass
 
